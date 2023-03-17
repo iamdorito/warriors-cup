78984c017af60d3ca9950230d3225ee6fc072c25
Dorothy <dorothychauu@gmail.com>
2023-03-17 17:15:56 -0400

    updated connect page

--- client/src/components/Connect.jsx
+++ client/src/components/Connect.jsx
@@ -34,10 +34,10 @@ export default function Connect() {
           {people.map((person) => (
             <li key={person.name}>
               <div className="flex items-center gap-x-6">
-                <img className="h-16 w-16 rounded-full" src={person.imageUrl} alt="" />
+                <img className="h-16 w-16 rounded-full"x src={person.imageUrl} alt="" />
                 <div>
                   <h3 className="text-base font-semibold leading-7 tracking-tight text-gray-900">{person.name}</h3>
-                  <p className="text-sm font-semibold leading-6 text-indigo-600">{person.role}</p>
+                  <p className="text-sm font-semibold leading-6 text-red-600">{person.role}</p>
                 </div>
               </div>
             </li>
