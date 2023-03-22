802467c06d9f6f2a19090d4dd65365b27eaa71c1
Dorothy <dorothychauu@gmail.com>
2023-03-22 17:36:53 -0400

    events route updated to show fight and fighter data

--- app/serializers/event_serializer.rb
+++ app/serializers/event_serializer.rb
@@ -1,6 +1,5 @@
 class EventSerializer < ActiveModel::Serializer
   attributes :id, :event_number, :name, :location, :date, :poster_img, :fights
 
-  # has_many :fights, serializer: FightSerializer
-  # has_many :fighters, through: :fights, serializer: FighterSerializer
+  has_many :fights, serializer: FightSerializer
 end
--- app/serializers/fighter_serializer.rb
+++ app/serializers/fighter_serializer.rb
@@ -1,4 +1,5 @@
 class FighterSerializer < ActiveModel::Serializer
   attributes :id, :first_name, :last_name, :nickname, :email, :gender, :wins, :losses, :draw, :headshot, :insta, :status, :gym_id, :division_id, :gym, :division
 
+    # has_many :fights, serializer: FightSerializer
 end
